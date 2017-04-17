afc9966748e15909de3312a320d73ea855f37d15
Doubleshot1122 <skibo1122@yahoo.com>
2017-04-16 19:31:42 -0700

    completed attendee id GET -updated the route and db sql -created new view

--- routes/attendees.js
+++ routes/attendees.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/connection.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EDM and Geek Events' });
+router.get('/:index', function(req, res, next) {
+  var id = req.params.index;
+  db('attendees').select('attendees.preferred_name as fName', 'attendees.last_name as lName', 'events.title as title', 'events.start_datetime as start', 'events.description as description', 'tickets.name as tName')
+    .innerJoin('tickets_attendees', 'attendees.id', 'tickets_attendees.attendee_id')
+    .innerJoin('tickets', 'tickets.id', 'tickets_attendees.ticket_id')
+    .innerJoin('events', 'events.id', 'tickets.event_id')
+    .where('attendees.id', id)
+  .then(attendeeEvents => {
+    console.log(attendeeEvents[0]);
+    console.log(attendeeEvents.length);
+    res.render('currAttendee', {attendeeEvents})
+  })
 });
 
 module.exports = router;
--- routes/events.js
+++ routes/events.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db/connection.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   db.raw("select events.id, events.title, events.description, venues.capacity, count(tickets.id) from events inner join venues on events.venue_id = venues.id inner join tickets on events.id = tickets.event_id inner join tickets_attendees on tickets_attendees.ticket_id = tickets.id inner join attendees on attendees.id = tickets_attendees.attendee_id group by events.id, tickets.id, events.title, events.description, venues.capacity;")
   .then(results => {
     results.rows.map(element =>{
@@ -23,5 +23,35 @@ router.get('/', function(req, res, next) {
     res.render('currEvents', {eventResults});
   })
 });
+
+router.get('/:index/register', (req, res, next) => {
+  var id = req.params.index;
+  db('events').select('events.title','events.description',
+  'venues.name as venueName', 'venues.line_1', 'venues.city', 'venues.state', 'venues.zip',
+  'tickets.name as ticketName', 'tickets.price as ticketPrice')
+    .innerJoin('venues', 'events.venue_id', 'venues.id')
+    .innerJoin('tickets', 'events.id', 'tickets.event_id')
+    .where('events.id', id)
+  .then(singleEvent => {
+    res.render('oneEvent', {singleEvent})
+  })
+})
+
+router.post('/', (req, res, next) =>{
+  var id = req.params.index;
+  var ticketType = req.body.ticketType;
+  var ticketSale = {
+    preferred_name: req.body.prefName,
+    last_name: req.body.lastName,
+    birthday: req.body.birthday,
+    email: req.body.email
+  }
+
+  if (!ticketSale.preferred_name) {
+    res.render('events/id/register', {error:'Name can not be blank', ticketSale})
+  } else {
+    db('attendees', 'id').insert
+  }
+})
 
 module.exports = router;
